aa7312f98031914ce48a118d457a6ea649f4681a
Hieumonki <hieumonki4@gmail.com>
2025-08-02 13:06:47 +0700

    push frontend lên GitHub

--- src/app/components/user/news/news.component.ts
+++ src/app/components/user/news/news.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../../../services/news.service';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-news',
+  standalone :true,
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+   imports: [ CommonModule]
 })
 export class NewsComponent implements OnInit {
   newsList: any[] = [];
